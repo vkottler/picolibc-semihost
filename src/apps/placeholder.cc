f868f1e2988bf49e36337de4f29487c468c15344
Vaughn Kottler <vaughnkottler@gmail.com>
2023-10-25 01:21:39 -0500

    Initial semihosted CLI assets

---
+++ src/apps/placeholder.cc
@@ -0,0 +1,30 @@
+/* internal */
+#include "App.h"
+
+bool state = true;
+
+void do_toggle(CommandLine &cli)
+{
+    (void)cli;
+    state = not state;
+    printf("Toggling state %s.\n", state ? "true" : "false");
+}
+
+void register_commands(CommandLineApp &app)
+{
+    app.add_handler("toggle", do_toggle, "toggle a boolean true or false");
+}
+
+char input[BUFSIZ];
+
+int main(void)
+{
+    App app(register_commands);
+
+    while (true)
+    {
+        app.poll_stdin(input);
+    }
+
+    return 0;
+}
--- src/retarget.c
+++ src/retarget.c
@@ -1,27 +1,75 @@
 /**
  * \file
- *
- * \brief Stubs for library hooks.
+ * \brief Some standard-library hook implementations.
  */
 
-/* toolchain */
-#include <stdio.h>
-
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-void _exit(int code) {
-  (void)code;
+/* toolchain */
+#include <semihost.h>
+#include <stdio.h>
 
-  while (1) {
-    ;
-  }
-}
+    void _exit(int code)
+    {
+        (void)code;
 
-FILE *const stdout = NULL;
-FILE *const stdin = NULL;
-FILE *const stderr = NULL;
+        while (1)
+        {
+            ;
+        }
+    }
+
+    /*
+     * Must be set by a debugger.
+     */
+    volatile bool enable_semihosting = false;
+
+    static int putc_wrapper(char c, FILE *file)
+    {
+        (void)file;
+
+        if (enable_semihosting)
+        {
+            char out[2] = {c, 0};
+            sys_semihost_write0(out);
+        }
+
+        return 0;
+    }
+
+    int stdin_fd = -1;
+
+    static int getc_wrapper(FILE *file)
+    {
+        (void)file;
+
+        int result = -1;
+
+        if (enable_semihosting && stdin_fd != -1)
+        {
+            char input = result;
+
+            /*
+             * The return value seems to be meaningless. There also does not
+             * seem to be a way to make this non-blocking.
+             */
+            sys_semihost_read(stdin_fd, &input, sizeof(char));
+
+            result = input;
+        }
+
+        return result;
+    }
+
+    static FILE __stdio =
+        FDEV_SETUP_STREAM(putc_wrapper, getc_wrapper, NULL, _FDEV_SETUP_RW);
+
+    FILE *const stdout = &__stdio;
+    FILE *const stdin = &__stdio;
+    FILE *const stderr = NULL;
 
 #ifdef __cplusplus
 }
